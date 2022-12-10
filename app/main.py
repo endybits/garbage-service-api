7ac87a04f32b116e0c5c932ce90e076ae95bb209
endybits <endyb.urbanista@gmail.com>
2022-12-10 08:41:11 -0500

    Get container details

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Path
 from fastapi import status
 
 from app.schemas.container import Container
@@ -15,6 +15,8 @@ async def home():
     return {'message': 'Hi, this is the incredible service project\'s home page'}
 
 
+
+### Create container
 @app.post(
     path='/containers',
     status_code=status.HTTP_201_CREATED,
@@ -33,7 +35,7 @@ async def create_container(
         f.write(container_json)
     return container
 
-
+### Get container list
 @app.get(
     path='/containers',
     status_code=status.HTTP_200_OK,
@@ -47,3 +49,22 @@ async def container_list(
         container_list = json.loads(container_list)
         f.close()
     return container_list
+
+### Get container detail
+@app.get(
+    path='/containers/{container_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=Container
+)
+async def container_detail(
+    container_id: int = Path(..., gt=0)
+):
+    with open('./app/containers.json', 'r', encoding='utf-8') as f:
+        container_list = f.read()
+        container_list = json.loads(container_list)
+        for container in container_list:
+            if container['container_id'] == container_id:
+                return container
+        f.close()
+    # TODO Handle errors when element doesn't exists (Raise a status code 400)
+    return {'Error': 'Element does not exists'}
